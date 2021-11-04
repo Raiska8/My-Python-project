7c2b639548f78eaf4cc58538f1eccbc4ae6f50cb
RAISA ABDRAI KYZY <87840244+Raiska8@users.noreply.github.com>
2021-11-04 22:20:55 +0300

    Add files via upload

---
+++ less1py.py
@@ -0,0 +1,14 @@
+print(5 * 6)
+print('Hello World!')
+print ('Привет Мир\n','My name Is Raiska')
+print(3**2)
+print(25//5)
+print(25%4)
+print (25-45.23)
+print(-23 - 12)
+
+name = input("What is your name?")
+print("Hello," , name)
+
+
+      
